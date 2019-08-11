084db5c763a9fe25b34191c9965332e26a9e3b2f
Greta <greta.r909@gmail.com>
2019-08-11 21:48:35 +1000

    Update fragment_tex_night.glsl

--- UNSWgraph-0.10/shaders/fragment_tex_night.glsl
+++ UNSWgraph-0.10/shaders/fragment_tex_night.glsl
@@ -20,7 +20,6 @@ uniform float phongExp;
 uniform sampler2D tex;
 
 uniform int torch;
-uniform vec3 torchlightDir;
 uniform vec3 torchlightIntensity;
 uniform vec3 torchlightPos;
 
@@ -30,7 +29,8 @@ in vec3 m;
 in vec2 texCoordFrag;
 
 void main()
-{	
+{
+	
 	if(torch == 0) {								// day mode
 	    vec3 m_unit = normalize(m);
 	    // Compute the s, v and r vectors
@@ -51,7 +51,6 @@ void main()
 	    vec4 ambientAndDiffuse = vec4(ambient + diffuse, 1);
 	
 	    outputColor = ambientAndDiffuse*input_color*texture(tex, texCoordFrag) + vec4(specular, 1);
-	    
     } else {											// night mode
     	vec3 m_unit = normalize(m);
 	    // Compute the s, v and r vectors
@@ -59,29 +58,15 @@ void main()
 	    vec3 s = normalize(view_matrix*vec4(torchlightPos, 0)).xyz; // camera
 	    vec3 v = normalize(-viewPosition.xyz);	//torchlightDir
 
+//	    vec3 s = normalize(view_matrix*vec4(torchlightPos,0)).xyz;
+//	    vec3 v = normalize(-viewPosition.xyz);
+
 	    vec3 r = normalize(reflect(-s,m_unit));
 	
 	    vec3 ambient = ambientIntensity*ambientCoeff;
-	    s = v; 			//normalize(vec3(0,0,1));
-	    vec3 diffuse;	// = max(torchlightIntensity*diffuseCoeff*dot(normalize(m_unit),s), 0.0);
+	    vec3 diffuse = max(torchlightIntensity*diffuseCoeff*dot(normalize(m_unit),s), 0.0);
 	    vec3 specular;
-	    
-	    
-	    float cosB = dot(normalize(viewPosition), vec4(0,0,-1,0));
-	    float B = acos(cosB);
-	    float cutoff = 20 * 3.14 / 180;
-	    
-	    float distance = length(viewPosition);
-	  
-	  	float distAttenuation = 100/(distance*distance);
-	    
-	    if (B < cutoff){
-	    	diffuse = max(torchlightIntensity*diffuseCoeff*dot(normalize(m_unit),s), 0.0)*pow(cosB, 20)*distAttenuation;
-	    }
-	    else {
-	    	diffuse = vec3(0,0,0);
-	    }
-	    
+	
 	    // Only show specular reflections for the front face
 	    if (dot(m_unit,s) > 0)
 	        specular = max(torchlightIntensity*specularCoeff*pow(dot(r,v),phongExp), 0.0);
@@ -90,6 +75,6 @@ void main()
 	
 	    vec4 ambientAndDiffuse = vec4(ambient + diffuse, 1);
 	
-	    outputColor = ambientAndDiffuse*input_color*texture(tex, texCoordFrag); //+ vec4(specular, 1);
+	    outputColor = ambientAndDiffuse*input_color*texture(tex, texCoordFrag) + vec4(specular, 1);
     }
 }
